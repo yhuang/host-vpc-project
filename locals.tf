a69dcf4f4e39d6d1ab2c1458f5a588f9a76dcfb0
Jimmy Huang <jimmy.huang@duragility.com>
2025-02-23 21:52:20 -0800

    enabled shared vpc features

--- locals.tf
+++ locals.tf
@@ -3,6 +3,7 @@ locals {
   gcp_folder_id                  = data.tfe_outputs.gcp_bootstrap.values.gcp_folder_id
   terraform_admin_gcp_project_id = data.tfe_outputs.gcp_bootstrap.values.terraform_admin_gcp_project_id
 
-  host_vpc_project_id = module.host_vpc_project.project_id
-  host_vpc_id         = google_compute_network.host_vpc.id
+  host_vpc_project_id        = module.host_vpc_project.project_id
+  shared_host_vpc_project_id = google_compute_shared_vpc_host_project.shared_host_project.id
+  shared_host_vpc_id         = google_compute_network.host_vpc.id
 }
--- network.tf
+++ network.tf
@@ -1,12 +1,16 @@
-resource "google_compute_network" "host_vpc" {
-  project = local.host_vpc_project_id
-  name    = "host-network"
+resource "google_compute_shared_vpc_host_project" "shared_host_project" {
+    project = local.host_vpc_project_id
 }
 
-resource "google_compute_subnetwork" "host_subnetwork_us_west1" {
-  project       = local.host_vpc_project_id
-  name          = "host-subnetwork-us-west1"
+resource "google_compute_network" "shared_host_vpc" {
+  project = local.shared_host_vpc_project_id
+  name    = "shared-host-network"
+}
+
+resource "google_compute_subnetwork" "shared_us_west1" {
+  project       = local.shared_host_vpc_project_id
+  name          = "shared-host-subnetwork-us-west1"
   ip_cidr_range = "10.2.0.0/16"
   region        = "us-west1"
-  network       = local.host_vpc_id
+  network       = local.shared_host_vpc_id
 }
